5229a7501bcb6cb10e54f4764382bedcc62c594f
najouaben <najouabenzahra2@gmail.com>
2023-03-15 14:03:22 +0100

    update test

--- test_py.py
+++ test_py.py
@@ -4,7 +4,7 @@ def test_print():
     assert app.print_function("hello") 
 
 def test_fun1():
-    assert app.function_1(2, 3)== 5
+    assert app.function_1(2,3) == 5
 
-def test_fun1():
-    assert app.function_2(5, 2)== 3
+def test_fun2():
+    assert app.function_2(5,2) == 3
